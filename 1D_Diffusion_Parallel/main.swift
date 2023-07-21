f597e7c9846902c1426a3aff0e92f47af44a0ff8
INDICUMA <101461638+INDICUMA@users.noreply.github.com>
2023-07-21 13:04:53 +0900

    Update main.swift

--- 1D_Diffusion_Parallel/main.swift
+++ 1D_Diffusion_Parallel/main.swift
@@ -4,32 +4,90 @@
 //
 //  Created by 조일현 on 2023/07/19.
 //
-
 import Foundation
 import MetalFFT
 
-let device = MTLCreateSystemDefaultDevice()!
-let commandQueue = device.makeCommandQueue()!
-let batchSize : Int = 28672
-let input = FFTSplitComplexBuffer(device: device, capacity: 128 * batchSize)
-let output = FFTComplexBuffer(device: device, capacity: 128 * batchSize)
+let batchSize = 1
+let n : Int = 128
+//fft, ifft array
+var fftInput = [SIMD2<Float>](repeating: SIMD2(1.0,0.0), count: n*batchSize)
+var fftoutput = [SIMD2<Float>](repeating: SIMD2(0.0,0.0), count: n*batchSize)
+var ifftInput = [SIMD2<Float>](repeating: SIMD2(0.0,0.0), count: n*batchSize)
+var ifftOutput = [SIMD2<Float>](repeating: SIMD2(0.0,0.0), count: n*batchSize)
 
-let fft = FastFourierTransform1D(device: device,
-                                 transformWidth: 128,
-                                 inputType: .splitComplex,
-                                 batchSize: batchSize)
+fftoutput = FFT1D(inputData : fftInput, n: n, batchSize: batchSize)
 
-print("execution time in milliseconds")
 
-for _ in 0..<20 {
+ifftInput = fftoutput
+
+
+ifftOutput = IFFT1D(inputData : ifftInput, n: n, batchSize: batchSize)
+for i in 0..<n*batchSize{
+    print("\(i)th", ifftOutput[i].x)
+}
+func FFT1D(inputData : [SIMD2<Float>] ,n : Int,batchSize : Int) -> [SIMD2<Float>]{
+    let device = MTLCreateSystemDefaultDevice()!
+    let commandQueue = device.makeCommandQueue()!
+    let numElement = n*batchSize
+    let inputByte = numElement*MemoryLayout<SIMD2<Float>>.stride
+    let input = FFTComplexBuffer(device: device, capacity: numElement)
+    let output = FFTComplexBuffer(device: device, capacity: numElement)
+    let fft = FastFourierTransform1D(device: device,
+                                                 transformWidth: n,
+                                                 inputType: .interleavedComplex,
+                                                 batchSize: batchSize)
+   // print("execution time in milliseconds")
     let commandBuffer = commandQueue.makeCommandBuffer()!
 
+    let inputDataComplex = (input.buffer.contents()+input.byteOffset).assumingMemoryBound(to: SIMD2<Float>.self)
+    //copy input from inputDataReal
+    memcpy(inputDataComplex, inputData, inputByte)
     fft.encode(commandBuffer: commandBuffer, input: input, output: output)
-
+    //fft
     commandBuffer.commit()
     commandBuffer.waitUntilCompleted()
+    var outputData = [SIMD2<Float>](repeating: SIMD2(0.0,0.0), count: numElement)
+    let outputDataComplex = output.buffer.contents().bindMemory(to: SIMD2<Float>.self, capacity: numElement)
+    for i in 0..<numElement{
+        outputData[i] = outputDataComplex[i]
+    }
+    /*time check
+     let elapsedTime = commandBuffer.gpuEndTime - commandBuffer.gpuStartTime
+     print(elapsedTime * 1e3)
+     */
+    return outputData
+}
 
-    let elapsedTime = commandBuffer.gpuEndTime - commandBuffer.gpuStartTime
-    print(elapsedTime * 1e3)
+func IFFT1D(inputData : [SIMD2<Float>] ,n : Int,batchSize : Int) -> [SIMD2<Float>]{
+    let device = MTLCreateSystemDefaultDevice()!
+    let commandQueue = device.makeCommandQueue()!
+    let numElement = n*batchSize
+    let inputByte = numElement*MemoryLayout<SIMD2<Float>>.stride
+    let input = FFTComplexBuffer(device: device, capacity: numElement)
+    let output = FFTComplexBuffer(device: device, capacity: numElement)
+    let fft = InverseFastFourierTransform1D(device: device,
+                                                 transformWidth: n,
+                                                 inputType: .interleavedComplex,
+                                                 batchSize: batchSize)
+   // print("execution time in milliseconds")
+    let commandBuffer = commandQueue.makeCommandBuffer()!
+
+    let inputDataComplex = (input.buffer.contents()+input.byteOffset).assumingMemoryBound(to: SIMD2<Float>.self)
+    //copy input from inputDataReal
+    memcpy(inputDataComplex, inputData, inputByte)
+    fft.encode(commandBuffer: commandBuffer, input: input, output: output)
+    //fft
+    commandBuffer.commit()
+    commandBuffer.waitUntilCompleted()
+    var outputData = [SIMD2<Float>](repeating: SIMD2(0.0,0.0), count: numElement)
+    let outputDataComplex = output.buffer.contents().bindMemory(to: SIMD2<Float>.self, capacity: numElement)
+    for i in 0..<numElement{
+        outputData[i] = outputDataComplex[i]
+    }
+    /*time check
+     let elapsedTime = commandBuffer.gpuEndTime - commandBuffer.gpuStartTime
+     print(elapsedTime * 1e3)
+     */
+    return outputData
 }
 
